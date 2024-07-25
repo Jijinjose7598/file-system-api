34496110c366c98ba7880dfe98bee7aa0513f15c
Jijin <jijinjose7598@gmail.com>
2024-07-25 15:47:25 +0530

    file system task

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const httpServer = express();
+const fs = require("fs");
+const path = require("path");
+
+//task 1
+
+httpServer.get("/", (req, res) => {
+  const timestamp = new Date().toISOString();
+  const filename = `${timestamp}.txt`.replace(/:/g, "-");
+
+  fs.writeFile(filename, timestamp, (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ message: "Error writing file", error: err });
+    }
+    res.status(200).json({ message: "File created successfully" });
+  });
+});
+httpServer.listen(3000, "localhost", () => {
+  console.log("server started", `http://localhost:3000`);
+});
+
+// task 2
+
+httpServer.get("/filter", (req, res) => {
+  fs.readdir("./files", (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Unable to scan directory" });
+    }
+
+    const textFiles = files.filter(
+      (file) => path.extname(file).toLowerCase() === ".txt"
+    );
+
+    if (files) {
+      console.log(textFiles);
+      return res.status(200).json({ success: "file filtered successfully" });
+    }
+  });
+});
+
